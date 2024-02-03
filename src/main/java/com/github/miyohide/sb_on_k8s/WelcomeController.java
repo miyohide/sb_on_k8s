ab12465910e233bf7459d0e8f76f6dd5d82b8f29
Hidenori Miyoshi <miyohide@gmail.com>
2024-02-03 21:24:43 +0900

    loggerを設定

--- src/main/java/com/github/miyohide/sb_on_k8s/WelcomeController.java
+++ src/main/java/com/github/miyohide/sb_on_k8s/WelcomeController.java
@@ -1,14 +1,17 @@
 package com.github.miyohide.sb_on_k8s;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class WelcomeController {
   @GetMapping("/")
   public Map<String, String> index() {
+    log.info("Welcome#indexにアクセスがありました");
     return Map.of(
             "message", "こんにちは",
             "page", "WelcomeController#index"

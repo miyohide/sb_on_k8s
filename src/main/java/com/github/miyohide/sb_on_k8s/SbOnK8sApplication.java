b3a64b1df78d58bcf3c8620fd0d0717c8e9d3130
Hidenori Miyoshi <miyohide@gmail.com>
2024-02-03 21:09:43 +0900

    新規プロジェクトを作成

---
+++ src/main/java/com/github/miyohide/sb_on_k8s/SbOnK8sApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.sb_on_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOnK8sApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbOnK8sApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/miyohide/sb_on_k8s/SbOnK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.sb_on_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbOnK8sApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

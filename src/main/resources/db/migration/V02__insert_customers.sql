0c05fac21ef009c15bdc41aabb4a31af55e50f47
Hidenori Miyoshi <miyohide@gmail.com>
2024-02-17 15:26:33 +0900

    insert文を追加

---
+++ src/main/resources/db/migration/V02__insert_customers.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers VALUES (1, 'f1', 'l1');
+INSERT INTO customers VALUES (2, 'f2', 'l2');
+INSERT INTO customers VALUES (3, 'f3', 'l3');
+INSERT INTO customers VALUES (4, 'f4', 'l4');
+INSERT INTO customers VALUES (5, 'f5', 'l5');

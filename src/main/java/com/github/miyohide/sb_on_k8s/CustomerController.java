b43ddfdcecf1e57a4c13eb7835862881a3c43714
Hidenori Miyoshi <miyohide@gmail.com>
2024-02-17 15:29:20 +0900

    ログを出力するようにした

--- src/main/java/com/github/miyohide/sb_on_k8s/CustomerController.java
+++ src/main/java/com/github/miyohide/sb_on_k8s/CustomerController.java
@@ -1,10 +1,12 @@
 package com.github.miyohide.sb_on_k8s;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class CustomerController {
   private CustomerRepository customerRepository;
@@ -15,6 +17,7 @@ public class CustomerController {
 
   @GetMapping("/customers")
   public List<Customer> index() {
+    log.info("/customersにGETアクセスがありました");
     return customerRepository.getAllCustomers();
   }
 }
